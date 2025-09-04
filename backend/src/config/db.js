271a6fe35865ddf5b8a091d1a250d86d896c4b95
Manoj <gdmh7991@gmail.com>
2025-09-04 09:04:03 +0530

    DB and port  setup completed

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDb connected:" ${conn.connection.host}`);
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log("MongoDb connected");
   } catch (error) {
     console.log(`Error:${error.message}`);
     process.exit(1);
---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+const express = require('express');
+
+const connectDB = require('./config/db')
+
+const app = express()
+
+// DB connection
+connectDB()
+
+// Start server
+const PORT = process.env.PORT || 5000
+app.listen(PORT,()=>console.log(`Server running on port ${PORT}`))
