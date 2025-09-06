5b5e8cdcd86646f813c0f1726668e7c9a7371ea1
Manoj <gdmh7991@gmail.com>
2025-09-06 13:46:37 +0530

    initial commit

--- backend/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const todoRoutes = require('./todoRoutes');
-const router = express.Router();
-
-router.use('/todo', todoRoutes);
-
-module.exports = router;
--- backend/src/routes/todoRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createTodo } = require('../controllers/todoController');
-
-router.post('/create', createTodo);
-
-module.exports = router;
